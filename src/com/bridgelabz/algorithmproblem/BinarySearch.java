762e44ecd47a2180a279eab44f77c751e773d08e
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-02-04 02:28:19 +0530

    Added : Welcome to Algorithms and Data Structure

---
+++ src/com/bridgelabz/algorithmproblem/BinarySearch.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.algorithmproblem;
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class BinarySearch {
+    public int binarySearch(String[] wordList, String searchName) {
+        int start = 0, length = wordList.length - 1;
+
+        while (start <= length) {
+            int middle = start + (length - start) / 2;
+            int result = searchName.compareTo(wordList[middle]);
+
+            if (result == 0)
+                return middle;
+
+            if (result > 0)
+                start = middle + 1;
+            else
+                length = middle - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        BinarySearch binaryObj = new BinarySearch();
+
+        Scanner scanner = new Scanner(System.in);
+        // create array list
+        String[] wordList = {"mohana", "sonu", "dinu", "arya", "soundu","saikumar"};
+        // array list is ascending order
+        Arrays.sort(wordList);
+        System.out.println("List of Words :");
+        System.out.println(Arrays.toString(wordList));
+        System.out.println("Enter the word you want to search");
+        String searchName = scanner.nextLine();
+        scanner.close();
+        int result = binaryObj.binarySearch(wordList, searchName);
+
+        if (result == -1) //word is found
+            System.out.println("word is found at "
+                    + "index " + result);
+        else
+            System.out.println("word is not present in the list");
+
+    }
+}
---
+++ src/com/bridgelabz/algorithmproblem/InsertionSort.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.algorithmproblem;
+
+public class InsertionSort {
+    public String[] sort(String array[]) {
+
+        String temp = " ";
+        int arrayLength = array.length;
+
+        for (int i = 0; i < arrayLength; i++) {
+            for (int j = i + 1; j < arrayLength; j++) {
+                if (array[i].compareToIgnoreCase(array[j]) > 0) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+
+    //main method
+    public static void main(String[] args) {
+        InsertionSort insertionSort = new InsertionSort();
+        // create array list
+        String[] array = {"de", "dee", "deep", "deepa", "deepak", "deepake", "deepaker", "deepakers"};
+
+        System.out.println("Before Insertion Sort");
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("   " + array[i]);
+        }
+        System.out.println();
+
+        System.out.println("After Insertion Sort");
+
+        String[] sortedArray = insertionSort.sort(array);
+
+        for (int i = 0; i < sortedArray.length; i++) {
+            System.out.print("   " + sortedArray[i]);
+        }
+    }
+}
